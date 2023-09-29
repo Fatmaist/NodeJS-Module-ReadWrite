6a60b6ff35f724e7ec0f5bd9a9b45e2295582e9c
fatmaist <fatmaistyn@gmail.com>
2023-09-29 20:31:45 +0700

    feat(rumus) : Create rumus luas dan keliling persegi panjang(rectangle)

--- module/rumus.js
+++ module/rumus.js
@@ -5,3 +5,11 @@ exports.luaspersegi = function(s) {
 exports.kelilingpersegi = function(s) {
     return 4 * s
 }
+
+exports.luaspersegipanjang = function(p, l) {
+    return p * l
+}
+
+exports.kelilingpersegipanjang = function(p, l) {
+    return 2 * (p + l)
+}
