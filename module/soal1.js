b3414c75b22c3bb692eecb694b5c6101de3e2814
fatmaist <fatmaistyn@gmail.com>
2023-09-29 20:51:44 +0700

    feat(module) : Create to call the function and output results

--- module/soal1.js
+++ module/soal1.js
@@ -1,3 +1,15 @@
 var calculator = require('./rumus.js')
 
 var s = 15, p = 30, l = 5
+
+console.log("\n------------------------ MENGHITUNG LUAS DAN KELILING PERSEGI ------------------------")
+
+console.log("\nLuas Persegi dengan sisi "+ s +" cm adalah     : " + calculator.luaspersegi(s) + " cm^2")
+
+console.log("\nKeliling Persegi dengan sisi "+ s +" cm adalah : " + calculator.kelilingpersegi(s) + " cm")
+
+console.log("\n-------------------- MENGHITUNG LUAS DAN KELILING PERSEGI PANJANG --------------------")
+
+console.log("\nLuas Persegi Panjang dengan panjang "+ p +" cm dan lebar " + l + " cm adalah     : " + calculator.luaspersegipanjang(p, l) + " cm^2")
+
+console.log("\nKeliling Persegi Panjang dengan panjang "+ p +" cm dan lebar " + l + " cm adalah : " + calculator.kelilingpersegipanjang(p, l) + " cm\n")
